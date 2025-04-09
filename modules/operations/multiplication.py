4163b49c4561532396dc52424469cdfdc1416611
João Souza <marceloamaral201924@gmail.com>
2025-04-08 23:44:46 -0400

    feat: multiplication function done

---
+++ modules/operations/multiplication.py
@@ -0,0 +1,14 @@
+def multiplicate_two_numbers(number1, number2) -> float:
+    """
+    function that does the multiplication between two numbers.
+
+    parameters:
+    number1 - number that will be multiplicated.
+    number2 - number that wil multiplicate.
+
+    return - the result of multiplication.
+    """
+
+    result = number1 * number2
+
+    return result
