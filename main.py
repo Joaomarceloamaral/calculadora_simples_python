7cfecd403fe25f2151d4493e2f73f8b3f9b9ea64
João Souza <marceloamaral201924@gmail.com>
2025-04-09 07:54:03 -0400

    feat: executing menu and the option selected
    calculadora simples ja funcionando, versão inicial

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from modules.menu_functions.menu_option import get_menu_option
+from modules.menu_functions.execute_option import execute_option
+
+
 def main():
 
-    pass
+    option = "0"
+    while option != "s":
+
+        option = get_menu_option()
+
+        result = execute_option(option)
+
+        if (option != "s") & (result is not False):
+
+            print(f"Resultado: {result}\n")
+
+    print("\n\nObrigado por utilizar minha calculadora! :)\n")
+    print("Saindo...\n")
 
 
 if __name__ == "__main__":
