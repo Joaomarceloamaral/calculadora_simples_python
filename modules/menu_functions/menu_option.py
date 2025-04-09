f9634d59f444cbf7af37c4fc74c3249cc9ae4fa3
João Souza <marceloamaral201924@gmail.com>
2025-04-09 08:00:42 -0400

    feat: function that shows the menu and get the option

---
+++ modules/menu_functions/menu_option.py
@@ -0,0 +1,13 @@
+def get_menu_option() -> str:
+
+    print("\n---------------------- Calculadora Simples ----------------------")
+
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("s - Sair")
+
+    option = str(input("\nEscolha uma das opções acima: "))
+
+    return option
